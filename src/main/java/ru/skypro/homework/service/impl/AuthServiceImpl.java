64a0ea0cb51a72d3179c865db1989b19d85a1ab6
roma <rommur5@mail.ru>
2024-02-21 23:14:12 +0300

    Реализовал авторизацию  и аутентификацию пользователя из БД. Смена пароля пользователя.

--- src/main/java/ru/skypro/homework/service/impl/AuthServiceImpl.java
+++ src/main/java/ru/skypro/homework/service/impl/AuthServiceImpl.java
@@ -5,19 +5,28 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.stereotype.Service;
+import ru.skypro.homework.dto.NewPassword;
 import ru.skypro.homework.dto.Register;
+import ru.skypro.homework.dto.Role;
+import ru.skypro.homework.mapper.UsersMapper;
+import ru.skypro.homework.model.UserEntity;
+import ru.skypro.homework.repository.UserRepository;
 import ru.skypro.homework.service.AuthService;
+
+import java.util.Optional;
 
 @Service
 public class AuthServiceImpl implements AuthService {
 
     private final UserDetailsManager manager;
     private final PasswordEncoder encoder;
+    private final UserRepository userRepository;
 
     public AuthServiceImpl(UserDetailsManager manager,
-                           PasswordEncoder passwordEncoder) {
+                           PasswordEncoder passwordEncoder, UserRepository userRepository) {
         this.manager = manager;
         this.encoder = passwordEncoder;
+        this.userRepository = userRepository;
     }
 
     @Override
@@ -30,18 +39,40 @@ public class AuthServiceImpl implements AuthService {
     }
 
     @Override
+    public Optional<String> changePassword(String username, String currentPassword, String newPassword) {
+        Optional<UserEntity> userOptional = userRepository.findByEmail(username);
+
+        if (userOptional.isPresent()) {
+            UserEntity user = userOptional.get();
+
+            if (encoder.matches(currentPassword, user.getPassword())) {
+                String newPasswordEncoded = encoder.encode(newPassword);
+                user.setPassword(newPasswordEncoded);
+                userRepository.save(user);
+                return Optional.of("Password changed successfully");
+            } else {
+                return Optional.of("Current password is incorrect");
+            }
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    @Override
     public boolean register(Register register) {
+        UserEntity user = UsersMapper.mapFromRegisterToUserEntity(register);
         if (manager.userExists(register.getUsername())) {
             return false;
         }
-        manager.createUser(
-                User.builder()
-                        .passwordEncoder(this.encoder::encode)
-                        .password(register.getPassword())
-                        .username(register.getUsername())
-                        .roles(register.getRole().name())
-                        .build());
+
+        user.setFirstName(register.getFirstName());
+        user.setLastName(register.getLastName());
+        user.setPhone(register.getPhone());
+        user.setPassword(encoder.encode(register.getPassword()));
+        user.setRole(register.getRole());
+        user.setEmail(register.getUsername());
+        userRepository.save(user);
+
         return true;
     }
-
 }
