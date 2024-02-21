183fa4a2b0a99d6295f9c06d6ce043ebdd27303b
roma <rommur5@mail.ru>
2024-02-21 23:14:12 +0300

    Реализовал авторизацию  и аутентификацию пользователя из БД. Смена пароля пользователя.

--- src/main/java/ru/skypro/homework/mapper/AdMapper.java
+++ src/main/java/ru/skypro/homework/mapper/AdMapper.java
@@ -1,16 +1,16 @@
 package ru.skypro.homework.mapper;
 
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import ru.skypro.homework.dto.AdDto;
 import ru.skypro.homework.model.AdEntity;
 
-@Component
+@Service
 public class AdMapper {
 
 //    Из Entity в DTO
     public AdDto mapToAdDTO(AdEntity adEntity) {
         AdDto adDto = new AdDto();
-        adDto.setPk(adEntity.getId());
+        adDto.setId(adEntity.getId());
         adDto.setAuthor(adEntity.getAuthor());
         adDto.setTitle(adEntity.getTitle());
         adDto.setDescription(adEntity.getDescription());
@@ -22,7 +22,7 @@ public class AdMapper {
 //    Из DTO в Entity
     public AdEntity mapToAdEntity(AdDto adDto) {
         AdEntity adEntity = new AdEntity();
-        adEntity.setId(adDto.getPk());
+        adEntity.setId(adDto.getId());
         adEntity.setAuthor(adDto.getAuthor());
         adEntity.setTitle(adDto.getTitle());
         adEntity.setDescription(adDto.getDescription());
--- src/main/java/ru/skypro/homework/mapper/UsersMapper.java
+++ src/main/java/ru/skypro/homework/mapper/UsersMapper.java
@@ -1,11 +1,24 @@
 package ru.skypro.homework.mapper;
 
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import ru.skypro.homework.dto.Register;
 import ru.skypro.homework.dto.User;
 import ru.skypro.homework.model.UserEntity;
 
-@Service
+@Component
 public class UsersMapper {
+    public static UserEntity mapFromRegisterToUserEntity(Register register) {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setEmail(register.getUsername());
+        userEntity.setRole(register.getRole());
+        userEntity.setPhone(register.getPhone());
+        userEntity.setFirstName(register.getFirstName());
+        userEntity.setLastName(register.getLastName());
+        userEntity.setPassword(register.getPassword());
+        return userEntity;
+    }
+
     //    из Entity в DTO
     public User mapToUserDTO(UserEntity userEntity) {
         User user = new User();
